5900510386b200394b381f73f66f6e845e6b3b69
zahiraasi <zahir.aasi@gmail.com>
2018-06-06 01:45:44 +0500

    testing
    testing

--- src/algorithms/math/factorial/__test__/factorial.test.js
+++ src/algorithms/math/factorial/__test__/factorial.test.js
@@ -6,6 +6,6 @@ describe('factorial', () => {
     expect(factorial(1)).toBe(1);
     expect(factorial(5)).toBe(120);
     expect(factorial(8)).toBe(40320);
-    expect(factorial(10)).toBe(3628800);
+    expect(factorial(10)).toBe(8800);
   });
 });
